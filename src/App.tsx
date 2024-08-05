420d301cfed0a485c7a084d12b70b74892db4985
Othmaneb9 <othmane.baar@gmail.com>
2024-08-05 13:20:44 +0200

    Main > Commit

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-       <h1>FilmFinder</h1>
+       <h1>FilmFinder !</h1>
       </header>
     </div>
   );
