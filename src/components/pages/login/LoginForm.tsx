07524ffa8309eacec35e6b439fd8eddbdd45c2cb
OthmaneB-Pro <othmane93.baar@gmail.com>
2024-08-06 14:33:57 +0200

    💄 LoginPage : Add display grid

--- src/components/pages/login/LoginForm.tsx
+++ src/components/pages/login/LoginForm.tsx
@@ -1,33 +1,52 @@
 import { useState } from "react";
+import styled from "styled-components";
 
 export default function LoginForm() {
-const [formValues, setFormValues] = useState({
-  username: '',
-  password: '',
-})
+  const [formValues, setFormValues] = useState({
+    username: "",
+    password: "",
+  });
 
-  const handleClick = (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+  const handleClick = (
+    event: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => {
     event.preventDefault();
-    console.log(formValues.username)
-    console.log(formValues.password)
-  }
+    console.log(formValues.username);
+    console.log(formValues.password);
+  };
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-      const {name, value} = event.target
+    const { name, value } = event.target;
 
-      setFormValues((prevValues) => ({...prevValues, [name]: value}))
-  }
-
+    setFormValues((prevValues) => ({ ...prevValues, [name]: value }));
+  };
 
   return (
-    <div>
+    <FormulaireStyled>
       <h2>Bienvenue chez FilmFinder</h2>
       <h3>Se connecter</h3>
       <form action="submit">
-        <input type="text" value={formValues.username} name="username" placeholder="Nom" onChange={handleChange}/>
-        <input type="password" value={formValues.password} name="password" placeholder="Mot de passe" onChange={handleChange}/>
+        <input
+          type="text"
+          value={formValues.username}
+          name="username"
+          placeholder="Nom"
+          onChange={handleChange}
+        />
+        <input
+          type="password"
+          value={formValues.password}
+          name="password"
+          placeholder="Mot de passe"
+          onChange={handleChange}
+        />
         <button onClick={handleClick}>Se Connecter</button>
         <p>Pas encore connecter ? S'inscrire</p>
       </form>
-    </div>
+    </FormulaireStyled>
   );
 }
+
+const FormulaireStyled = styled.div`
+  background: red;
+  
+`
--- src/components/pages/login/LoginPage.tsx
+++ src/components/pages/login/LoginPage.tsx
@@ -1,16 +1,36 @@
-import styled from 'styled-components'
-import LoginForm from './LoginForm'
-import LoginImage from './LoginImage'
+import styled from "styled-components";
+import LoginForm from "./LoginForm";
+import LoginImage from "./LoginImage";
 
 export default function LoginPage() {
   return (
     <LoginPageStyle>
-        <LoginImage/>
-        <LoginForm/>
+      <div className="container">
+        <LoginImage />
+        <LoginForm />
+      </div>
     </LoginPageStyle>
-  )
+  );
 }
 
 const LoginPageStyle = styled.div`
-    background: red;
-`
+  background: #2996d9;
+  height: 100vh;
+  background-size: cover;
+  overflow-y: hidden;
+  
+
+  .container{
+    background: white;
+    width: 65%;
+    height: 70vh;
+    position: relative;
+    margin-left: 300px;
+    margin-top: 150px;
+    border-radius: 15px;
+    overflow: hidden;
+
+    display: grid;
+    grid-template-columns: 2fr 1fr;
+  }
+`;
