dfc82a17ea95568678cebba04d65f14662c8906c
OthmaneB-Pro <othmane93.baar@gmail.com>
2024-08-08 20:23:41 +0200

    🍱 MainPage Update assets for categories

--- src/components/pages/main/MainPage.tsx
+++ src/components/pages/main/MainPage.tsx
@@ -2,14 +2,26 @@ import styled from "styled-components";
 import FooterMainPage from "./footer/FooterMainPage";
 import HeaderMain from "./header/HeaderMain";
 import CatalogMain from "./catalog/CatalogMain";
+import { useState } from "react";
+import { GenreType } from "./catalog/categories/Categories";
+import {MoviesContext} from "../../../context/MoviesContext";
 
 export default function MainPage() {
+const [movieGenre, setMovieGenre] = useState<GenreType[]>([]);
+
+  const moviesContextValue = {
+    movieGenre,
+    setMovieGenre,
+  }
+
   return (
+    <MoviesContext.Provider value={moviesContextValue}>
     <MainPageStyled>
       <HeaderMain />
       <CatalogMain/>
       <FooterMainPage />
     </MainPageStyled>
+    </MoviesContext.Provider>
   );
 }
 
