336f7cea1fe552588f48b1baa927c001df1f7359
OthmaneB-Pro <othmane93.baar@gmail.com>
2024-08-14 15:39:52 +0200

    ♻️ Search : Separating the result to make the code cleaner

--- src/components/pages/searchButton/searchButton/SearchButtonPage.tsx
+++ src/components/pages/searchButton/searchButton/SearchButtonPage.tsx
@@ -1,21 +1,14 @@
-import { useState, useEffect, useContext } from "react";
-import { searchMoviesByTitle } from "../../../../api/moviedb"; // Assurez-vous que le chemin est correct
+// SearchButtonPage.tsx
+import { useState, useEffect } from "react";
+import { searchMoviesByTitle } from "../../../../api/moviedb";
 import { PopularMovieType } from "../../main/catalog/PopularMovieAndSeries";
-import CardPrimary from "../../../reusable-ui/CardPrimary";
-import {
-  CarouselStyled,
-  ScrollableContainer,
-} from "../../../reusable-styles/CardPrimaryStyles";
-import { useNavigate, useParams } from "react-router-dom";
-import { FavoriteList } from "../../../../context/UserContext";
 import SearchInputButton from "./SearchInputButton";
+import SearchResults from "./SearchResults"; // Importez le nouveau composant
+import styled from "styled-components";
 
 export default function SearchButtonPage() {
-  const { username } = useParams();
-  const { onAddFavorite } = useContext(FavoriteList);
   const [name, setName] = useState<string>("");
   const [results, setResults] = useState<PopularMovieType[]>([]);
-  const navigate = useNavigate();
 
   useEffect(() => {
     const fetchResults = async () => {
@@ -23,11 +16,9 @@ export default function SearchButtonPage() {
         setResults([]);
         return;
       }
-
       try {
         const searchResults = await searchMoviesByTitle(name);
         setResults(searchResults);
-        console.log(results);
       } catch (error) {
         console.error("Search failed:", error);
       }
@@ -41,28 +32,13 @@ export default function SearchButtonPage() {
   }, [name]);
 
   return (
-    <div>
-      <SearchInputButton name={name} setName={setName} /> 
-
-      <ScrollableContainer>
-        <CarouselStyled>
-          {results.map((movie) => (
-            <CardPrimary
-              key={movie.id}
-              title={movie.title}
-              image={movie.poster_path}
-              label={movie.overview}
-              date={movie.release_date}
-              onClick={() => {
-                navigate(`/details/${username}/${movie.id}`);
-              }}
-              onFavorite={() => {
-                onAddFavorite(movie);
-              }}
-            />
-          ))}
-        </CarouselStyled>
-      </ScrollableContainer>
-    </div>
+    <SearchButtonStyled>
+      <SearchInputButton name={name} setName={setName} />
+      <SearchResults results={results} />
+    </SearchButtonStyled>
   );
 }
+
+const SearchButtonStyled = styled.div`
+  margin-bottom: 199px;
+`
